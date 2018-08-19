8e21eda76680e751e742279f35b91f2ae837ae04
Son Tran <sontd.it@gmail.com>
2018-08-19 14:00:37 +0700

    Update rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/about', to: 'home#index'
+  get '/contact', to: 'home#index'
 
   namespace :api, format: :json do
     resources :tasks, except: [:edit, :new]
