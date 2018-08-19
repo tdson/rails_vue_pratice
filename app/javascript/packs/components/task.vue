f183eaee7ea76d3ccf240999864a37b54af4cb11
Son Tran <sontd.it@gmail.com>
2018-08-19 09:39:44 +0700

    v-for and child component

---
+++ app/javascript/packs/components/task.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <li class="collection-item">
+    <input type="checkbox" :id="`task-${task.id}`"/>
+    <label :for="`task-${task.id}`" v-text="task.title"/>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['task'],
+
+  methods: {
+    handleChange () {
+      this.$emit('task-checked', this.task)
+    }
+  }
+}
+</script>
--- app/javascript/packs/pages/home.vue
+++ app/javascript/packs/pages/home.vue
@@ -2,31 +2,13 @@
   <div>
     <div>
       <ul class="collection">
-        <li id="row_task_1" class="collection-item">
-          <input type="checkbox" id="task_1" />
-          <label for="task_1">Sample Task</label>
-        </li>
-        <li id="row_task_2" class="collection-item">
-          <input type="checkbox" id="task_2" />
-          <label for="task_2">Sample Task</label>
-        </li>
-        <li id="row_task_3" class="collection-item">
-          <input type="checkbox" id="task_3" />
-          <label for="task_3">Sample Task</label>
-        </li>
+        <task v-for="task in finishedTasks" :key="task.id" :task="task"/>
       </ul>
     </div>
     <div class="btn">Display finished tasks</div>
     <div id="finished-tasks" class="display_none">
       <ul class="collection">
-        <li id="row_task_4" class="collection-item">
-          <input type="checkbox" id="'task_4" checked="checked" />
-          <label v-bind:for="task_4" class="line-through">Done Task</label>
-        </li>
-        <li id="row_task_5" class="collection-item">
-          <input type="checkbox" id="'task_5" checked="checked" />
-          <label v-bind:for="task_5" class="line-through">Done Task</label>
-        </li>
+        <task v-for="task in unfinishedTasks" :key="task.id" :task="task"/>
       </ul>
     </div>
   </div>
@@ -34,12 +16,17 @@
 
 <script>
 import axios from 'axios'
+import Task from '../components/task'
 
 export default {
   data: function () {
     return {
       tasks: []
     }
+  },
+
+  components: {
+    Task
   },
 
   computed: {
